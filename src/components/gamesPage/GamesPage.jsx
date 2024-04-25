a6ac7a8bb8a8689e767f3bb230788f93ce965232
Allen Buenaventura <115561893+allenbnvtra@users.noreply.github.com>
2024-04-25 10:13:29 +0800

    done

--- src/components/gamesPage/GamesPage.jsx
+++ src/components/gamesPage/GamesPage.jsx
@@ -8,21 +8,18 @@ import Link from "next/link";
 const helpMessages = {
   sudoku: {
     id: "sudoku",
-    message: `Objective: The goal of Sudoku is to fill a 9x9 grid with numbers so that each row, column, and 3x3 subgrid contains all of the digits from 1 to 9.
-    Starting Grid: Sudoku puzzles start with some cells already filled with numbers. These initial numbers provide a starting point for solving the puzzle.
-    Rules:
-    Each row must contain all numbers from 1 to 9 without repetition.
-    Each column must contain all numbers from 1 to 9 without repetition.
-    Each 3x3 subgrid (or region) must contain all numbers from 1 to 9 without repetition.
-    Solving Strategy:
-    Look for rows, columns, or regions with fewest missing numbers. These are typically easier to solve.
-    Identify numbers that can only fit in one cell within a row, column, or region. This is often referred to as "elimination."
-    Use logical deduction and elimination to gradually fill in the grid. If you're stuck, try making educated guesses and continue from there.
-    Trial and Error: Sometimes, you may need to try different numbers in a cell to see which one fits. If a number conflicts with the rules, backtrack and try a different number.
-    Patience and Practice: Sudoku requires patience and practice. Start with easier puzzles and gradually move to more challenging ones as you become more comfortable with the rules and strategies.
-    Remember, there's always a unique solution to every Sudoku puzzle, and no math or arithmetic is required. Enjoy the challenge of solving Sudoku puzzles!`,
+    message: `Instructions:
+    - The goal of Sudoku is to fill a 9x9 grid with numbers so that each row, column, and 3x3 subgrid contains all of the digits from 1 to 9.
+    - Sudoku puzzles start with some cells already filled with numbers. These initial numbers provide a starting point for solving the puzzle.
+    - Each row must contain all numbers from 1 to 9 without repetition.
+    - Each column must contain all numbers from 1 to 9 without repetition.
+    - Each 3x3 subgrid (or region) must contain all numbers from 1 to 9 without repetition.
+    - Look for rows, columns, or regions with fewest missing numbers. These are typically easier to solve.
+    - Identify numbers that can only fit in one cell within a row, column, or region. This is often referred to as "elimination."
+    - Use logical deduction and elimination to gradually fill in the grid. If you're stuck, try making educated guesses and continue from there.
+    - Sometimes, you may need to try different numbers in a cell to see which one fits. If a number conflicts with the rules, backtrack and try a different number.
+    - Patience and practice are key. Start with easier puzzles and gradually move to more challenging ones as you become more comfortable with the rules and strategies. Enjoy the challenge of solving Sudoku puzzles!`,
   },
-  // Add more games with their respective help messages here
 };
 
 const GamesPage = () => {
@@ -53,27 +50,14 @@ const GamesPage = () => {
             imgUrl={Sudoku}
             gameUrl="sudoku"
             gameTitle="Sudoku Puzzle"
-            helpMessage="Objective: The goal of Sudoku is to fill a 9x9 grid with numbers so that each row, column, and 3x3 subgrid contains all of the digits from 1 to 9.
-            Starting Grid: Sudoku puzzles start with some cells already filled with numbers. These initial numbers provide a starting point for solving the puzzle.
-            Rules:
-            Each row must contain all numbers from 1 to 9 without repetition.
-            Each column must contain all numbers from 1 to 9 without repetition.
-            Each 3x3 subgrid (or region) must contain all numbers from 1 to 9 without repetition.
-            Solving Strategy:
-            Look for rows, columns, or regions with fewest missing numbers. These are typically easier to solve.
-            Identify numbers that can only fit in one cell within a row, column, or region. This is often referred to as elimination.
-            Use logical deduction and elimination to gradually fill in the grid. If you're stuck, try making educated guesses and continue from there.
-            Trial and Error: Sometimes, you may need to try different numbers in a cell to see which one fits. If a number conflicts with the rules, backtrack and try a different number.
-            Patience and Practice: Sudoku requires patience and practice. Start with easier puzzles and gradually move to more challenging ones as you become more comfortable with the rules and strategies.
-            Remember, there's always a unique solution to every Sudoku puzzle, and no math or arithmetic is required. Enjoy the challenge of solving Sudoku puzzles!"
+            helpMessage={helpMessages.sudoku.message}
           />
           <GameCards
             imgUrl={Millionaire}
             gameUrl="who-wants-to-be-a-millionaire"
             gameTitle="Math Millionaire"
-            helpMessage="Welcome to Math Millionaire!
-            Objective: Answer a series of math questions correctly to progress through the game and win the million-dollar prize!
-            Rules:
+            helpMessage="Instructions:
+            - Answer a series of math questions correctly to progress through the game and win the million-dollar prize!
             - You will be presented with a series of multiple-choice math questions.
             - Select the correct answer from the options provided.
             - Answer as many questions as you can correctly to increase your winnings.
@@ -86,15 +70,14 @@ const GamesPage = () => {
             imgUrl={Shapes}
             gameUrl="shape"
             gameTitle="Shape It"
-            helpMessage={`
-            Objective: Identify the shape shown on the screen correctly to earn points and progress through the game.\n
-            Rules:\n
-            - You will be presented with a shape displayed on the screen.\n
-            - Choose the correct shape from the three options provided below/beside the image.\n
-            - Select the option that matches the shape shown as accurately as possible.\n
-            - Be careful! Choosing the wrong shape may result in a loss of points or progress.\n
-            - Answer as many questions as you can correctly to achieve a high score.\n
-            - Use your knowledge of shapes and geometrical properties to make accurate choices.\n
+            helpMessage={`Instructions:
+            - Identify the shape shown on the screen correctly to earn points and progress through the game.
+            - You will be presented with a shape displayed on the screen.
+            - Choose the correct shape from the three options provided below/beside the image.
+            - Select the option that matches the shape shown as accurately as possible.
+            - Be careful! Choosing the wrong shape may result in a loss of points or progress.
+            - Answer as many questions as you can correctly to achieve a high score.
+            - Use your knowledge of shapes and geometrical properties to make accurate choices.
             Get ready to test your shape recognition skills and have fun playing the game!`}
           />
         </div>
